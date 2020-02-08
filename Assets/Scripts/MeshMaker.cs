272effccfe09ce8eb74ce5e5484f677f5db9ee2b
egc2846 <egc2846@rit.edu>
2020-02-08 13:44:31 -0500

    Added skeletons to MeshMaker.cs

--- Assets/Scripts/MeshMaker.cs
+++ Assets/Scripts/MeshMaker.cs
@@ -3,25 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+//using defaultNamespace;
 
-public class MeshMaker : MonoBehaviour
+public class MeshMaker
 {
-    // Start is called before the first frame update
+    private float[][] bwImage = FlatGenNoise.CalcNoise();
     void Start()
     {
-        Mesh flatMesh = new Mesh();
-        Vector3[] vertexArray = new Vector3[100];
+              
+        Mesh flatMesh = new Mesh();        //Instantiate new Mesh
+        Vector3[] vertexArray = convertImage(bwImage);    
         flatMesh.vertices = vertexArray;
     }
 
-    Vector3[] convertImage(Texture2D aa)
+    Vector3[] convertImage(float[][] bwImage)
     {
+        foreach (var number in bwImage) //TODO Parallel foreach loop
+        {
+            
+        }
+
         return null;
     }
 
-    // Update is called once per frame
-    // void Update()
-    // {
-    //     
-    // }
+    public float[][] GetbwImage()
+    {
+        return bwImage;
+    }
 }
