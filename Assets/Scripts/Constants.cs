9b141e44971fa8f9e2ef8b2760cd51020dd244cf
Ionis <iak2023@rit.edu>
2020-02-08 13:24:19 -0500

    Created the viewer required constants

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public static class Constants
@@ -7,4 +8,6 @@ public static class Constants
     public static int ChunkHeight = 1000;
     public static int ChunkWidth = 1000;
     public static int RenderDistance = 3;
+    public static float MouseSensitivity = 100.0f;
+    public static float MovementSpeed = 100.0f;
 }
