b02b38dd75db6ae320d144070275b9ad8b2f0075
jcolley0226 <jcolley0226@gmail.com>
2020-02-08 15:14:16 -0500

    Made method makeColor except I didn't put mad nested for loops. Lets forget about the time when I did that. It's a little embarrassing. Thanks

--- Assets/Scripts/FlatTexturing.cs
+++ Assets/Scripts/FlatTexturing.cs
@@ -23,36 +23,30 @@ namespace defaultNamespace
         //colors according to height
         public Texture2D makeColor()
         {
-            for(int i = 0; i < bwImage.Length; i++)
-            {
-                for(int j = 0; j < bwImage.Length; j++)
-                {
-                    for (int height = 0; height < Constants.ChunkHeight; height += 1)
-                    {
-                        for (int width = 0; width < Constants.ChunkWidth; width += 1)
-                        {
 
-                            if (bwImage[i, j] < 75.0) //20,20,20
-                            {
-                                Pixels[(int)height * Constants.ChunkHeight + (int)width] = new Color(20,20,20 );
-                            }
-                            else if (bwImage[i, j] < 155.0) //140,62,14
-                            {
-                                Pixels[(int)height * Constants.ChunkHeight + (int)width] = new Color(140,62,14 );
-                            }
-                            else if (bwImage[i, j] < 185.0) //10,145,15
-                            {
-                                Pixels[(int)height * Constants.ChunkHeight + (int)width] = new Color(10,145,15 );
-                            }
-                            else //203,243,255
-                            {
-                                Pixels[(int)height * Constants.ChunkHeight + (int)width] = new Color(203,243,255 );
-                            }
-                        }
+            for (int height = 0; height < Constants.ChunkHeight; height += 1)
+            {
+                for (int width = 0; width < Constants.ChunkWidth; width += 1)
+                {
+                    var value = bwImage[height, width];
+                    if (value < 75.0) //20,20,20
+                    {
+                        Pixels[(int) height * Constants.ChunkHeight + (int) width] = new Color(20, 20, 20);
+                    }
+                    else if (value < 155.0) //140,62,14
+                    {
+                        Pixels[(int) height * Constants.ChunkHeight + (int) width] = new Color(140, 62, 14);
+                    }
+                    else if (value < 185.0) //10,145,15
+                    {
+                        Pixels[(int) height * Constants.ChunkHeight + (int) width] = new Color(10, 145, 15);
+                    }
+                    else //203,243,255
+                    {
+                        Pixels[(int) height * Constants.ChunkHeight + (int) width] = new Color(203, 243, 255);
                     }
                 }
             }
-            
             Colors.SetPixels(Pixels);
             Colors.Apply();
             return Colors;
