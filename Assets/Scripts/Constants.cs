cc2270f811ff885bd3779de991438a4139e4f869
Takatsuka-mark <takatsuka.mark@gmail.com>
2020-02-08 13:20:00 -0500

    Adding in constants class

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants
+{
+    public static int ChunkHeight = 1000;
+    public static int ChunkWidth = 1000;
+    public static int RenderDistance = 3;
+}
