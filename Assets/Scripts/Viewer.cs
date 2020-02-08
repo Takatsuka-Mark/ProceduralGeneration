4f42c136f9991d50dc128c127621f2f7a50fc481
Ionis <iak2023@rit.edu>
2020-02-08 13:45:15 -0500

    Look controls are working correctly

--- Assets/Scripts/Viewer.cs
+++ Assets/Scripts/Viewer.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class Viewer : MonoBehaviour
 {
+    public Transform viewerTransform;
+    public GameObject viewer;
+
+    private float rotX = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rotateViewer();
     }
 
-    Vector3 getLookDir(GameObject viewer)
+    void rotateViewer()
+    {
+        var rotY = Input.GetAxis("Mouse X") * Constants.MouseSensitivity * Time.deltaTime;
+        Debug.Log(rotY);
+        rotX += Input.GetAxis("Mouse Y") * Constants.MouseSensitivity * Time.deltaTime;
+        rotX = Mathf.Clamp(rotX, -90f, 90f);
+        viewerTransform.Rotate(Vector3.up * rotY);
+        viewer.transform.localRotation = Quaternion.Euler(-rotX, 0f, 0f);
+    }
+    Vector3 getLookDir()
     {
         
         return Vector3.zero;
