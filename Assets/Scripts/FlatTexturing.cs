c8f4661436c570c576b539a89de9b9497fd10b13
jcolley0226 <jcolley0226@gmail.com>
2020-02-08 14:13:39 -0500

    added constructor the to FlatTexturing.cs class

--- Assets/Scripts/FlatTexturing.cs
+++ Assets/Scripts/FlatTexturing.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//Takes float array representing colors 0-255 (black and white) and outputs a colored texture 2d
-public class FlatTexturing
+namespace defaultNamespace
 {
-    //Takes in 2d array of nums 0-255 representing colors from black to white and returns texture2d of full range of 
-    //colors according to height
-    public Texture2D makeColor()
+    
+    //Takes float array representing colors 0-255 (black and white) and outputs a colored texture 2d
+    public class FlatTexturing
     {
-        return null;
-    }
-    
-    
-    
+        private float[,] bwImage;
+        private Texture2D _colors;
+        
+        public FlatTexturing(){ 
+            bwImage = MeshMaker.GetbwImage();
+        }
+        
+        //Takes in 2d array of nums 0-255 representing colors from black to white and returns texture2d of full range of 
+        //colors according to height
+        public Texture2D makeColor()
+        {
+            var noiseMap = new FlatGenNoise();
+            
+            return _colors;
+        }
 
+
+
+
+    }
 }
