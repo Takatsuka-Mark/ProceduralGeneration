71ae326cfd9b0974b957636b65627db59176344b
jcolley0226 <jcolley0226@gmail.com>
2020-02-08 13:20:01 -0500

    Actually Added FlatTexturing class. Disregard earlier push lel

---
+++ Assets/Scripts/FlatTexturing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Takes in texture 2d (black and white) and outputs a colored texture 2d
+public class Flat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
