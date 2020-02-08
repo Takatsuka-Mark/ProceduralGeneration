9684ae6cfccf186435471751abfecb0d74976d56
Ionis <iak2023@rit.edu>
2020-02-08 15:52:17 -0500

    Camera controls and autogeneration works correctly

--- Assets/Scripts/Viewer.cs
+++ Assets/Scripts/Viewer.cs
@@ -1,17 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using defaultNamespace;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class Viewer : MonoBehaviour
 {
-    public Transform viewerTransform;
-    public GameObject viewer;
+    private Transform _viewerTransform;
+    private GameObject _root;
+    private GameObject _cam;
+    private GameObject _light;
+    private Camera _viewer;
+    private Rigidbody _rb;
+    private Light _viewerLight;
+    
 
     private float rotX = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
+        init();
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -19,6 +28,38 @@ public class Viewer : MonoBehaviour
     void Update()
     {
         rotateViewer();
+        viewerMove();
+    }
+
+    void init()
+    {
+        _root = new GameObject();
+        _rb = _root.AddComponent(typeof(Rigidbody)) as Rigidbody;
+        _rb.useGravity = false;
+        _viewerTransform = _root.transform;
+        _root.name = "Viewer Group";
+        
+        _cam = new GameObject();
+        _viewer = _cam.AddComponent(typeof(Camera)) as Camera;
+        System.Diagnostics.Debug.Assert(_viewer != null, nameof(_viewer) + " != null");
+        var transform1 = _viewer.transform;
+        var rootTransform = _root.transform;
+        transform1.position = rootTransform.position;
+        transform1.rotation = _root.transform.rotation;
+        transform1.parent = _root.transform;
+        _viewer.name = "ViewerCamera";
+
+        _light = new GameObject();
+        _viewerLight = _light.AddComponent(typeof(Light)) as Light;
+        System.Diagnostics.Debug.Assert(_viewerLight != null, nameof(_viewerLight) + " != null");
+        var transform2 = _viewerLight.transform;
+        // ReSharper disable once Unity.InefficientPropertyAccess
+        transform2.position = rootTransform.position;
+        transform2.rotation = rootTransform.rotation;
+        transform2.parent = rootTransform;
+        _viewerLight.name = "ViewerLight";
+
+
     }
 
     void rotateViewer()
@@ -26,13 +67,22 @@ public class Viewer : MonoBehaviour
         var rotY = Input.GetAxis("Mouse X") * Constants.MouseSensitivity * Time.deltaTime;
         rotX += Input.GetAxis("Mouse Y") * Constants.MouseSensitivity * Time.deltaTime;
         rotX = Mathf.Clamp(rotX, -90f, 90f);
-        viewerTransform.Rotate(Vector3.up * rotY);
-        viewer.transform.localRotation = Quaternion.Euler(-rotX, 0f, 0f);
+        _viewerTransform.Rotate(Vector3.up * rotY);
+        _viewer.transform.localRotation = Quaternion.Euler(-rotX, 0f, 0f);
     }
-    Vector3 getLookDir()
+    void viewerMove()
     {
-        Vector3 lookDir = viewerTransform.eulerAngles;
-        Debug.Log(lookDir);
-        return Vector3.zero;
+        var trueForward = _viewer.transform.forward + _viewerTransform.forward;
+        trueForward = trueForward.normalized;
+        
+        // ReSharper disable once CompareOfFloatsByEqualityOperator
+        if (Math.Abs(Input.GetAxisRaw("Vertical")) != 0)
+        {
+            _rb.velocity = trueForward * (Constants.MovementSpeed);
+        }
+        else
+        {
+            _rb.velocity = Vector3.zero;
+        }
     }
 }
