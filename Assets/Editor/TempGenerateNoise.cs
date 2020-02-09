df504c692548b91f824b10e35fc4543305b77eb1
Takatsuka-mark <takatsuka.mark@gmail.com>
2020-02-08 19:10:53 -0500

    Missed Upload on file generation

---
+++ Assets/Editor/TempGenerateNoise.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(DisplayNoise))]
+public class TempGenerateNoise : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DisplayNoise displayNoise = (DisplayNoise) target;
+
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Generate"))
+        {
+            displayNoise.generateNoise();
+            
+        }
+    }
+}
