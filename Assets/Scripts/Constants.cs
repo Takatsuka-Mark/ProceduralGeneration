8d5ac5a5d4bf87e57f06813476aa2b2812d6845f
Takatsuka-mark <takatsuka.mark@gmail.com>
2020-02-08 19:09:40 -0500

    Changed Noise Generation

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -6,11 +6,11 @@ namespace defaultNamespace
 {
     public static class Constants
     {
-        public static int ChunkHeight = 50;
-        public static int ChunkWidth = 50;
+        public static int ChunkHeight = 250;
+        public static int ChunkWidth = 250;
         public static int RenderDistance = 3;
         public static float MouseSensitivity = 100.0f;
         public static float MovementSpeed = 10.0f;
-        public static int Seed = 1;
+        public static int Seed = 255;
     }
 }
