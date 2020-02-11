e6c3d17f72e7c86239bb09cb03a1f29d4aedb676
Takatsuka-mark <takatsuka.mark@gmail.com>
2020-02-11 13:21:18 -0500

    Starting to create world

---
+++ Assets/Scripts/World/Planet.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class Planet : MonoBehaviour
+{
+    [Range(2, 256)] public int resolution = 10;
+
+    public ShapeSettings ShapeSettings;
+    public ColorSettings ColorSettings;
+
+    private ShapeGenerator _shapeGenerator;
+    
+    [SerializeField, HideInInspector]    // this hides, but also allows them to be saved.
+    private MeshFilter[] meshFilters;
+    private TerrainFace[] _terrainFaces;
+
+    // This is an event function to work with the editor
+    private void OnValidate()
+    {
+        init();
+        GenerateMesh();
+    }
+
+    void init()
+    {
+        _shapeGenerator = new ShapeGenerator(ShapeSettings);
+        // we only want to create new filters the first time
+        if (meshFilters == null || meshFilters.Length == 0)
+        {
+            meshFilters = new MeshFilter[6];     // one for each face
+        }
+
+        _terrainFaces = new TerrainFace[6];
+        // up, down, left, right, forward, back
+        Vector3[] dir = {Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back}; 
+
+        for (int i = 0; i < 6; i++)
+        {
+            if (meshFilters[i] == null)
+            {
+                GameObject meshObj = new GameObject("mesh");
+                meshObj.transform.parent = transform;
+
+                meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+                meshFilters[i] = meshObj.AddComponent<MeshFilter>();
+                meshFilters[i].sharedMesh = new Mesh();
+            }
+            _terrainFaces[i] = new TerrainFace(_shapeGenerator, meshFilters[i].sharedMesh, resolution, dir[i]);
+        }
+        
+    }
+
+    public void GeneratePlanet()
+    {
+        init();
+        GenerateMesh();
+        GenerateColors();
+    }
+
+    public void OnColorSettingsUpdated()
+    {
+        init();
+        GenerateColors();
+    }
+
+    public void OnShapeSettingsUpdated()
+    {
+        init();
+        GenerateMesh();
+    }
+
+    void GenerateMesh()
+    {
+        foreach (var terrainFace in _terrainFaces)
+        {
+            terrainFace.GenerateMesh();
+        }
+    }
+
+    void GenerateColors()
+    {
+        foreach (var meshFilter in meshFilters)
+        {
+            meshFilter.GetComponent<MeshRenderer>().sharedMaterial.color = ColorSettings.color;
+        }
+    }
+}
---
+++ Assets/Scripts/World/TerrainFace.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainFace
+{
+    private Mesh mesh;
+    private int resolution;
+    private Vector3 normalVector;
+    private Vector3 axisA;
+    private Vector3 axisB;
+    private ShapeGenerator shapeGenerator;
+
+    public TerrainFace(ShapeGenerator shapeGenerator, Mesh mesh, int resolution, Vector3 normalVector)
+    {
+        this.mesh = mesh;
+        this.resolution = resolution;
+        this.normalVector = normalVector;
+        this.shapeGenerator = shapeGenerator;
+
+        axisA = new Vector3(normalVector.y, normalVector.z, normalVector.x);
+        axisB = Vector3.Cross(normalVector, axisA);
+    }
+
+
+    public void GenerateMesh()
+    {
+        Vector3[] vertices = new Vector3[resolution * resolution];
+        int[] triangles = new int[((resolution - 1) * (resolution - 1)) * 6];    // this is the number of verticies on the triangles
+        int triIndex = 0;
+
+        for (int y = 0; y < resolution; y++)
+        {
+            for (int x = 0; x < resolution; x++)
+            {
+                Vector2 percent = new Vector2(x, y) / (resolution - 1);    // how close to complete each of the loops are
+                Vector3 pointOnUnitCube =
+                    normalVector + (percent.x - 0.5f) * 2 * axisA +
+                    (percent.y - 0.5f) * 2 * axisB;    // like a unit circle, but it is the world cube
+                Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
+
+
+                var i = y * resolution + x;
+                // vertices[i] = pointOnUnitCube;      // generates a cube
+                // vertices[i] = pointOnUnitSphere;    // generates a sphere
+                vertices[i] = shapeGenerator.CalcPointOnPlanet(pointOnUnitSphere);    // generates sphere with radius
+
+                // this will prevent us from counting triangles 1 to the right, and below the mesh
+                if (x != resolution - 1 && y != resolution - 1)
+                {
+                    
+                    // these are the triangles that are part of the square on the mesh we are looking at
+                    // triangle 1
+                    triangles[triIndex] = i;
+                    triangles[triIndex + 1] = i + resolution + 1;
+                    triangles[triIndex + 2] = i + resolution;
+                    
+                    // triangle 2
+                    triangles[triIndex + 3] = i;
+                    triangles[triIndex + 4] = i + 1;
+                    triangles[triIndex + 5] = i + resolution + 1;
+
+                    triIndex += 6;
+                }
+            }
+        }
+
+        mesh.Clear();    // when vertices are mapped at lower res, the triangles have incorrect indexing, unless cleared
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+}
